43d04aa9be54e57d958fcb2efa8046f25528520f
GiantLuigi4 <tjmferet@hotmail.com>
2020-09-21 16:33:55 -0400

    start unifying mojmap with intermediary

--- src/test/java/com/tfc/mappings/Test.java
+++ src/test/java/com/tfc/mappings/Test.java
@@ -13,40 +13,61 @@ import java.util.Date;
 public class Test {
 	public static void main(String[] args) throws IOException {
 		for (int i = 14; i <= 16; i++) {
-			long startMap = new Date().getTime();
-			Holder holder = Intermediary.generate("1." + i);
-			long endMap = new Date().getTime();
-			System.out.println("Mapping version 1." + i + " took: " + (endMap - startMap) + " ms");
-			long startWrite = new Date().getTime();
-			File f = new File("com.tfc.mappings\\inter\\1." + i + "_formatted.txt");
-			f.getParentFile().mkdirs();
-			f.createNewFile();
-			FileOutputStream stream = new FileOutputStream(f);
-			assert holder != null;
-			stream.write(holder.toFancyString().getBytes());
-			stream.close();
-			long endWrite = new Date().getTime();
-			System.out.println("Writing version 1." + i + " took: " + (endWrite - startWrite) + " ms");
-			long startFind = new Date().getTime();
-			String clazz = (holder.getFromPrimaryName("net/minecraft/class_1593").fancyString());
-			long endFind = new Date().getTime();
-			System.out.println("Finding phantom for version 1." + i + " took: " + (endFind - startFind) + " ms\n");
-			f = new File("com.tfc.mappings\\inter\\1." + i + "_phantom_class.txt");
-			f.getParentFile().mkdirs();
-			f.createNewFile();
-			stream = new FileOutputStream(f);
-			stream.write(clazz.getBytes());
-			stream.close();
+			try {
+				long startMap = new Date().getTime();
+				Holder holder = Intermediary.generate("1." + i);
+				long endMap = new Date().getTime();
+				System.out.println("Mapping version 1." + i + " took: " + (endMap - startMap) + " ms");
+				long startWrite = new Date().getTime();
+				File f = new File("mappings\\inter\\1." + i + "_formatted.txt");
+				f.getParentFile().mkdirs();
+				f.createNewFile();
+				FileOutputStream stream = new FileOutputStream(f);
+				assert holder != null;
+				stream.write(holder.toFancyString().getBytes());
+				stream.close();
+				long endWrite = new Date().getTime();
+				System.out.println("Writing version 1." + i + " took: " + (endWrite - startWrite) + " ms");
+				long startFind = new Date().getTime();
+				String clazz = (holder.getFromPrimaryName("net/minecraft/class_1593").fancyString());
+				long endFind = new Date().getTime();
+				System.out.println("Finding phantom for version 1." + i + " took: " + (endFind - startFind) + " ms\n");
+				f = new File("mappings\\inter\\1." + i + "_phantom_class.txt");
+				f.getParentFile().mkdirs();
+				f.createNewFile();
+				stream = new FileOutputStream(f);
+				stream.write(clazz.getBytes());
+				stream.close();
+			} catch (Throwable ignored) {
+			}
+			try {
+				long startMap = new Date().getTime();
+				Holder holder = Mojang.generate("1." + i);
+				long endMap = new Date().getTime();
+				System.out.println("Mapping version 1." + i + " took: " + (endMap - startMap) + " ms");
+				long startWrite = new Date().getTime();
+				File f = new File("mappings\\mojmap\\1." + i + "_formatted.txt");
+				f.getParentFile().mkdirs();
+				f.createNewFile();
+				FileOutputStream stream = new FileOutputStream(f);
+				assert holder != null;
+				stream.write(holder.toFancyString().getBytes());
+				stream.close();
+				long endWrite = new Date().getTime();
+				System.out.println("Writing version 1." + i + " took: " + (endWrite - startWrite) + " ms");
+				long startFind = new Date().getTime();
+				String clazz = (holder.getFromPrimaryName("net/minecraft/world/entity/monster/Phantom").fancyString());
+				long endFind = new Date().getTime();
+				System.out.println("Finding phantom for version 1." + i + " took: " + (endFind - startFind) + " ms\n");
+				f = new File("mappings\\mojmap\\1." + i + "_phantom_class.txt");
+				f.getParentFile().mkdirs();
+				f.createNewFile();
+				stream = new FileOutputStream(f);
+				stream.write(clazz.getBytes());
+				stream.close();
+			} catch (Throwable ignored) {
+			}
 		}
-		Holder holder1 = Mojang.generate("1.16");
-		File f = new File("com.tfc.mappings\\mojmap\\1.16_formatted.txt");
-		f.getParentFile().mkdirs();
-		f.createNewFile();
-		FileOutputStream stream = new FileOutputStream(f);
-		assert holder1 != null;
-		stream.write(holder1.toFancyString().getBytes());
-		stream.close();
-		Class block = holder1.getFromPrimaryName("net.minecraft.world.level.block.Block"); //primary name is real name, secondary is proguarded
 //		for (Method m : block.getMethods())
 //			System.out.println(m.getDesc());
 //		for (Field field : block.getFields())
